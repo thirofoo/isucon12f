c1d1fbbc711f6b6dbea8318f494cf78412c23b5c
Daaaai0809 <dai.tsuruga@80and.co>
2023-11-24 14:45:29 +0900

    複合indexに変更

--- go/main.go
+++ go/main.go
@@ -662,11 +662,7 @@ func initialize(c echo.Context) error {
 		return errorResponse(c, http.StatusInternalServerError, err)
 	}
 
-	query := "USE `isucon`; CREATE INDEX `idx_user_id_present_history` ON `user_present_all_received_history` (`user_id`);"
-	if _, err := dbx.Exec(query); err != nil {
-		return errorResponse(c, http.StatusInternalServerError, err)
-	}
-	query = "USE `isucon`; CREATE INDEX `idx_present_all_id` ON `user_present_all_received_history` (`present_all_id`);"
+	query := "USE `isucon`; CREATE INDEX `idx_user_id_present_history` ON `user_present_all_received_history` (`user_id`, `present_all_id`);"
 	if _, err := dbx.Exec(query); err != nil {
 		return errorResponse(c, http.StatusInternalServerError, err)
 	}
