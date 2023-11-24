f3c49d522dc178ec4af3455bec941edb200e71dc
Daaaai0809 <dai.tsuruga@80and.co>
2023-11-24 13:45:14 +0900

    add index

--- sql/init.sh
+++ sql/init.sh
@@ -36,3 +36,9 @@ echo "LOAD DATA INFILE '${SECURE_DIR}5_user_presents_not_receive_data.tsv' REPLA
         --host "$ISUCON_DB_HOST" \
         --port "$ISUCON_DB_PORT" \
         "$ISUCON_DB_NAME" 
+
+echo "create index user_id_present_all_id on user_present_all_received_history" | mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME" < 7_create_index.sql
